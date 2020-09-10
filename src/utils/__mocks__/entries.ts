4d2f04afa7b8814a923f04b64b2338f00203a9df
MarkusTryban <markustryban@gmail.com>
2020-09-09 20:43:53 -0400

    Return mocked entrie

---
+++ src/utils/__mocks__/entries.ts
@@ -0,0 +1,9 @@
+export default jest.fn(() => {
+  return {
+    title: 'Mocked title',
+    description: 'Mocked description',
+    programType: 'Mocked programType',
+    images: 'Mocked images',
+    releaseYear: 'Mocked releaseYear',
+  };
+});
