c388775570eeff258466ff57a8b24d1c5dc062f2
MarkusTryban <markustryban@gmail.com>
2020-09-09 20:53:44 -0400

    Update import of App to MovieStream

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/App/MovieStream';
+import MovieStream from './components/App/MovieStream';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <MovieStream />
   </React.StrictMode>,
   document.getElementById('root')
 );
