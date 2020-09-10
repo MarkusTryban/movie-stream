68dc8d30b7b86e0d9558cff89c13b0bce37f8bfb
MarkusTryban <markustryban@gmail.com>
2020-09-09 21:08:23 -0400

    Return h1 from MovieStream

--- src/components/App/MovieStream.tsx
+++ src/components/App/MovieStream.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import './App.css';
 
-function App() {
+import './MovieStream.css';
+
+const MovieStream = () => {
   return (
     <div className='App'>
       <h1>Movie Stream</h1>
     </div>
   );
-}
+};
 
-export default App;
+export default MovieStream;
