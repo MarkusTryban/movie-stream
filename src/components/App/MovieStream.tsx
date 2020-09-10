a30f7dd7f63d95604168859aa2d0545dc010ff9a
MarkusTryban <markustryban@gmail.com>
2020-09-10 14:22:43 -0400

    Return span with movie entry data

--- src/components/App/MovieStream.tsx
+++ src/components/App/MovieStream.tsx
@@ -1,13 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+
+import { entriesArray } from '../../utils/entries';
 
 import './MovieStream.css';
 
-const MovieStream = () => {
-  return (
-    <div className='App'>
-      <h1>Movie Stream</h1>
-    </div>
-  );
-};
+class MovieStream extends Component {
+  GET_ENTRY: void;
+
+  constructor(props: Readonly<{}>) {
+    super(props);
+
+    this.state = {
+      entryData: [],
+    };
+    this.GET_ENTRY = entriesArray();
+    console.log(entriesArray);
+  }
+
+  getMovieEntry = () => {
+    fetch(this.GET_ENTRY)
+      .then((res) => res.json())
+      .then((result) => {
+        this.setState({
+          entryData: result,
+        });
+      });
+  };
+
+  render() {
+    const { entryData } = this.state;
+    return (
+      <div>
+        <span>{entryData}</span>
+      </div>
+    );
+  }
+}
 
 export default MovieStream;
