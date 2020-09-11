271463fa4e59dd572843d81c4798c29fd87e6767
MarkusTryban <markustryban@gmail.com>
2020-09-11 19:13:33 -0400

    Return main footer

--- src/components/App/MovieStream.tsx
+++ src/components/App/MovieStream.tsx
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 
 import { entriesArray } from '../../utils/entries';
+
 import MainContent from '../MainContent/MainContent';
+import MainFooter from '../MainFooter/MainFooter';
 import MainHeader from '../MainHeader/MainHeader';
 
 import './MovieStream.css';
@@ -22,6 +24,7 @@ class MovieStream extends Component {
       <>
         <MainHeader />
         <MainContent />
+        <MainFooter />
       </>
     );
   }
