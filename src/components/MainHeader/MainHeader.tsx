dd00aaaae484ef66eca00c41569a637091d804d8
MarkusTryban <markustryban@gmail.com>
2020-09-11 18:42:21 -0400

    Add main-title & btn-container

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -5,9 +5,13 @@ import './MainHeader.css';
 const MainHeader = () => {
   return (
     <div className='main-header'>
-      <h1>Movie Streaming</h1>
-      <button id='btn-login'>Log in</button>
-      <button id='btn-trial'>Strat Trial</button>
+      <div id='main-title'>
+        <h1>Movie Streaming</h1>
+      </div>
+      <div className='btn-container'>
+        <button id='btn-login'>Log in</button>
+        <button id='btn-trial'>Strat Trial</button>
+      </div>
     </div>
   );
 };
