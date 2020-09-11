de59c9f5de48d70483fd8b523d287f1c1f7dd369
MarkusTryban <markustryban@gmail.com>
2020-09-11 17:28:21 -0400

    Return main header div

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -1,15 +1,11 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class MainHeader extends Component {
-  render() {
-    return (
-      <div className='main-header'>
-        <h1>Movie Streaming</h1>
-        <button id='btn-login'>Log in</button>
-        <button id='btn-trial'>Strat Trial</button>
-      </div>
-    );
-  }
-}
+const MainHeader = () => {
+  <div className='main-header'>
+    <h1>Movie Streaming</h1>
+    <button id='btn-login'>Log in</button>
+    <button id='btn-trial'>Strat Trial</button>
+  </div>;
+};
 
 export default MainHeader;
