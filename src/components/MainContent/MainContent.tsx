52db16bd680b9f678bef6480a2cfe489363478e0
MarkusTryban <markustryban@gmail.com>
2020-09-11 19:05:17 -0400

    Create main content component

---
+++ src/components/MainContent/MainContent.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import MainHeader from '../MainHeader/MainHeader';
+
+const MainContent = () => {
+  return (
+    <div className='content-container'>
+      <div id='title-container'>
+        <h2>Popular Titles</h2>
+      </div>
+      <div id='main-content'>
+        <span>Main Content</span>
+      </div>
+    </div>
+  );
+};
+
+export default MainContent;
