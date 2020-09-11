4f5e62c29dc812f827ab0f3fb09eb709be0f086b
MarkusTryban <markustryban@gmail.com>
2020-09-11 17:41:34 -0400

    Fix return statment

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 const MainHeader = () => {
-  <div className='main-header'>
-    <h1>Movie Streaming</h1>
-    <button id='btn-login'>Log in</button>
-    <button id='btn-trial'>Strat Trial</button>
-  </div>;
+  return (
+    <div className='main-header'>
+      <h1>Movie Streaming</h1>
+      <button id='btn-login'>Log in</button>
+      <button id='btn-trial'>Strat Trial</button>
+    </div>
+  );
 };
 
 export default MainHeader;
