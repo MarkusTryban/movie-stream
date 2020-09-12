12437ac7c58ca2245b004418f954792ae747cc63
MarkusTryban <markustryban@gmail.com>
2020-09-12 16:15:01 -0400

    Change folder App to MovieStream

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import MovieStream from './components/App/MovieStream';
+import MovieStream from './components/MovieStream/MovieStream';
 
 ReactDOM.render(
   <React.StrictMode>
