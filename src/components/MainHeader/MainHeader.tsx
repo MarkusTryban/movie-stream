43bbe6ee85d7df8dcf6e7c144a6bcb3ff91903e4
MarkusTryban <markustryban@gmail.com>
2020-09-13 17:46:35 -0400

    Return main header of component

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -5,12 +5,10 @@ import './MainHeader.css';
 const MainHeader = () => {
   return (
     <div className='main-header'>
-      <div id='main-title'>
-        <h1>Movie Streaming</h1>
-        <h3>Popular Titles</h3>
-        <button id='btn-login'>Log in</button>
-        <button id='btn-trial'>Strat Trial</button>
-      </div>
+      <h1>Movie Streaming</h1>
+      <h2>Popular Titles</h2>
+      <button id='btn-login'>Log in</button>
+      <button id='btn-trial'>Strat Trial</button>
     </div>
   );
 };
