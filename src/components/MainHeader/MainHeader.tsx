afbaaf5beb99a022cc031edab0cd3c9f86305768
MarkusTryban <markustryban@gmail.com>
2020-09-13 14:05:57 -0400

    Return main header content

--- src/components/MainHeader/MainHeader.tsx
+++ src/components/MainHeader/MainHeader.tsx
@@ -7,9 +7,7 @@ const MainHeader = () => {
     <div className='main-header'>
       <div id='main-title'>
         <h1>Movie Streaming</h1>
-      </div>
-      <div id='movie-title'>Popular Titles</div>
-      <div className='btn-container'>
+        <h3>Popular Titles</h3>
         <button id='btn-login'>Log in</button>
         <button id='btn-trial'>Strat Trial</button>
       </div>
