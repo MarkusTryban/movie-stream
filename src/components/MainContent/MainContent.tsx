bc98549b4cf0e003be897a50cef87e9aadf09259
MarkusTryban <markustryban@gmail.com>
2020-09-14 17:07:01 -0400

    Update main contnet

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -5,7 +5,15 @@ import './MainContent.css';
 const MainContent = () => {
   return (
     <div className='content-container'>
-      <div id='main-content'>Movie Stream</div>
+      <div>One</div>
+      <div>Two</div>
+      <div>Three</div>
+      <div>Four</div>
+      <div>Five</div>
+      <div>Six</div>
+      <div>Seven</div>
+      <div>Eight</div>
+      <div>Ten</div>
     </div>
   );
 };
