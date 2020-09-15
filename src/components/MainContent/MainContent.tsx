717c09b106ed0ced9a8a901b5756619649c91684
MarkusTryban <markustryban@gmail.com>
2020-09-15 16:35:38 -0400

    Return 3 divs

--- src/components/MainContent/MainContent.tsx
+++ src/components/MainContent/MainContent.tsx
@@ -5,15 +5,9 @@ import './MainContent.css';
 const MainContent = () => {
   return (
     <div className='content-container'>
-      <div>One</div>
-      <div>Two</div>
-      <div>Three</div>
-      <div>Four</div>
-      <div>Five</div>
-      <div>Six</div>
-      <div>Seven</div>
-      <div>Eight</div>
-      <div>Ten</div>
+      <div className='one'>One</div>
+      <div className='one'>Two</div>
+      <div className='one'>Three</div>
     </div>
   );
 };
